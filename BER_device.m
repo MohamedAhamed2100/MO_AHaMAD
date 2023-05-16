90954de9896f56d263f7fef713e1ef3c3c0c985f
MohamedAhamed2100 <107947222+MohamedAhamed2100@users.noreply.github.com>
2023-05-16 23:37:10 +0300

    Delete BER_device.m

--- BER_device.m
+++
@@ -1,5 +0,0 @@
-function [BER,num_errors] = BER_device(o_lineCodeVec ,lineCodeVec)
-    % count the number of errors
-    num_errors = sum(o_lineCodeVec ~= lineCodeVec);
-    BER = num_errors / length(lineCodeVec);
-end
