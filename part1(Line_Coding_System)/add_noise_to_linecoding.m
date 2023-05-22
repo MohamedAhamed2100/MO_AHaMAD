ab2b32bd21200b12e7212f16d1032ec1b66b1bd6
MohamedAhamed2100 <107947222+MohamedAhamed2100@users.noreply.github.com>
2023-05-22 17:05:17 +0300

    Delete add_noise_to_linecoding.m

--- part1(Line_Coding_System)/add_noise_to_linecoding.m
+++
@@ -1,8 +0,0 @@
-function received_signal_with_noise = add_noise_to_linecoding( lineCode ,sigma ,Vectime )
-  % define your time vector and maximum supply voltage
-  t = Vectime;
-  % noise
-  n = sigma*randn(1,length(t));     
-  % add the noise to your received signal
-  received_signal_with_noise = lineCode + n;
-end
